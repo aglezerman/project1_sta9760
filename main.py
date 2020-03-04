0e7933d4de7826d6065480d5c532c5f111ba0aa9
andreg <andreglezerman@gmail.com>
2020-03-04 15:38:16 -0500

    updated notes in py files

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+## import dependencies from known python packages
 import requests
 import sys
 import argparse
 import os
+## import functions from src subfolder
 from src.api import get_data
 from src.output import show_data
 
+# create variables to be passed in command line
 variables = argparse.ArgumentParser()
-
+# page size is required
 variables.add_argument('--page_size', type = int, required = True)
+# num pages is not required, if not provided will ask for all of the data
 variables.add_argument('--num_pages', type = int, default = -1)
+# output is not required. if provided, stdout will print to a file
 variables.add_argument('--output', type = str, default = -1)
 
 args = variables.parse_args()
--- src/output.py
+++ src/output.py
@@ -2,15 +2,19 @@ from sodapy import Socrata
 
 DATABASE_ID = 'nc67-uf89'
 
+# when called, this function will print the data to a file
 def print_to_file(data,output:str):
     with open(output, 'a') as f:
         print(data, file=f)
         
 def show_data(client,page_size: int, num_pages: int, output: str):
     ## if NUM_PAGES was not provided, set it to display all of the data
-    numPages = int(num_pages)
-    pageSize = int(page_size)
-    if numPages == -1: # -1 is the default
+    try:
+        numPages = int(num_pages)
+        pageSize = int(page_size)
+    except:
+        print("Input error. enter num pages and page size as integers (whole number)")
+    if numPages == -1: # -1 is the default. if no input was given, set num pages to ALL pages 
         dataset_size = int(client.get(DATABASE_ID, select ='COUNT(*)')[0]['COUNT'])
         numPages = int(dataset_size/pageSize)
     ## if output was not set, print to stdout
