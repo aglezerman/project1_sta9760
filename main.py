122728270e359c9e65b216ac7610fd0f8bf10a11
andreg <andreglezerman@gmail.com>
2020-03-15 18:04:51 -0400

    fixed double text

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ## import dependencies from known python packages
 import requests
 import sys
@@ -35,41 +34,3 @@ if output == 'es':
 	push(data_list,es)
 else:
 	show_data(client, page_size, num_pages, output)
-=======
-## import dependencies from known python packages
-import requests
-import sys
-import argparse
-import os
-## import functions from src subfolder
-from src.api import get_data
-from src.output import show_data
-#
-from src.esearch import create_and_update_index, push
-
-# create variables to be passed in command line
-variables = argparse.ArgumentParser()
-# page size is required
-variables.add_argument('--page_size', type = int, required = True)
-# num pages is not required, if not provided will ask for all of the data
-variables.add_argument('--num_pages', type = int, default = -1)
-# output is not required. if provided, stdout will print to a file
-variables.add_argument('--output', type = str, default = -1)
-
-args = variables.parse_args()
-
-# app key environment variable 
-app_token = os.environ['APP_KEY']
-
-page_size = args.page_size
-num_pages = args.num_pages
-output = args.output
-
-client = get_data(app_token)
-if output == 'es':
-	data_list = show_data(client, page_size, num_pages, output)
-	es = create_and_update_index('parking-violation-index')
-	push(data_list,es)
-else:
-	show_data(client, page_size, num_pages, output)
->>>>>>> 837c8c58d5001a6837ccdbeca4ba3bcee341fc7b
